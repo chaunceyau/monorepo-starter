870843a2cbf711e2ac347811f0f566365095cf41
Austin <chauncey.au@gmail.com>
2021-04-25 20:17:43 -0700

    feature: scaffold basic account page

---
+++ apps/next-starter/pages/account/index.tsx
@@ -0,0 +1,9 @@
+import { TopNavigationLayout } from 'apps/next-starter/components/layouts/top-nav';
+
+export default function Account() {
+  return (
+    <TopNavigationLayout title="Account Settings" session={null} router={null}>
+      <h1>lolmldsmafldsmalfmd</h1>
+    </TopNavigationLayout>
+  );
+}
